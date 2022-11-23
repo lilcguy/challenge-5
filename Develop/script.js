4bc30f62efa5e7e5cda08e75ab0ca516deefc3eb
unknown <thecbradshaw89@gmail.com>
2022-11-22 22:11:19 -0600

    added some more comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 $("#currentDay").text(moment().format('MMMM Do YYYY')); //o is the ordinal (nd)
+//jquery target; sets its text equal to the current date with the help of moment, in the format specified
 
 $(".saveBtn").on('click', function(){ //jquery syntax for event listener
-var time = $(this).parent().attr("id");//'this' refers to the event target (i.e. which button) that has been clicked. this prevents having to do an event listener for each button with a different id.
+var time = $(this).parent().attr("id");//'this' refers to the event target (.saveBtn) and specifically which button that has been clicked. this prevents having to do an event listener for each button with a different id.
 var desc = $(this).siblings(".description").val();
-localStorage.setItem(time, desc);
+localStorage.setItem(time, desc); //saves it to local storage. key-value pairs.
 
 });
 
@@ -18,17 +19,17 @@ $("#16 .description").val(localStorage.getItem('16'));
 $("#17 .description").val(localStorage.getItem('17'));
 
 function timeCheck(){
-var currentHour = moment().hour();
+var currentHour = moment().hour(); //gets the current hour with the help of moment, and then saves it in a variable that can be referenced
 //var currentHour = 12; //test for functionality. result: success
 
 $(".time-block").each(function(){ //jquery syntax for a for loop
-    var blockHour = parseInt($(this).attr("id")); //parseInt takes any string it gets and converts to integer if possible
+    var blockHour = parseInt($(this).attr("id")); //parseInt takes any string it gets and converts to integer if possible. this allows it to be compared to currentHour
     
-    if (blockHour < currentHour){ //past
+    if (blockHour < currentHour){ //check for past
         $(this).removeClass("present");
         $(this).removeClass("future");
         $(this).addClass("past"); 
-    } else if (currentHour === blockHour){ //present
+    } else if (currentHour === blockHour){ //check for present
         $(this).removeClass("past"); //order matters: wipe everything, and THEN add the new class, which triggers render of the dom
         $(this).removeClass("future");
         $(this).addClass("present");
